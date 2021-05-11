6a728cfbc742b9baec0f9e3eea0dae5b0e3aafb8
marlonecobos <manubio13@gmail.com>
2021-05-10 23:26:24 -0500

    v 0.0.3 pca_raster general improvement

--- R/pca_raster.R
+++ R/pca_raster.R
@@ -1,205 +1,208 @@
-#' Principal componens for raster layers and projections
+#' Principal component analysis for raster layers
 #'
 #' @description pca_raster performs a principal component analysis with a set of
-#' variables and produces raster layers of them. If needed the pricipal
-#' components are projected to other scenarios.
+#' variables and returns results including PCs as raster layers. If needed,
+#' principal components can be projected to other scenarios.
 #'
-#' @param variables (character) name of the folder where environmental variables
+#' @param variables RasterStack or character. If RasterStack, stack of layers to
+#' be used; if character, name of the folder where environmental variables
 #' are.
-#' @param in_format (character) format of variables in \code{variables}. Options
-#' are "ascii", "GTiff", and "EHdr" = bil. Default = "ascii".
-#' @param out_format (character) format of variables to be written in distinct
-#' sets inside \code{output_directory}. Options are "ascii", "GTiff", and
-#' "EHdr" = bil. Default = "ascii".
-#' @param scale (logical) wheter or not to scale variables while performing
+#' @param in_format (character) if class of \code{variables} = "character",
+#' format of variables in such a folder. Options are "ascii", "GTiff", and
+#' "EHdr" = bil. Default = NULL.
+#' @param scale (logical) whether or not to scale variables while performing
 #' principal component analyses.
+#' @param center (logical) whether or not to center variables while performing
+#' principal component analyses.
+#' @param n_pcs (numeric) number of principal components to be returned as
+#' raster layers. By default all principal components are returned.
 #' @param project (logical) whether or not to project the species niche to other
-#' scenario(s). If TRUE, argument \code{projection_variables} needs to be defined.
+#' scenario(s). If TRUE, argument \code{projection_variables} must be defined.
 #' Default = FALSE.
-#' @param projection_variables (character or RasterStack) if character, name of
-#' the folder where subfolders with environmental variables of scenarios for
-#' projections are (useful if multiple projections are needed). If RasterStack,
-#' object containing stacked variables of only one projection scenario.
-#' Variables must correspond with variables in \code{variables} (i.e., their name
-#' must correspond but they should represent conditions in other scenario).
-#' @param return_back (logical) whether or not return raster layers of principal
-#' components to the R environment in a list with other results.
-#' @param n_pcs (numeric) number of principal components to be returned as rasters.
-#' By default all principal components are returned as RasterLayers.
+#' @param projection_variables character or RasterStack. If character, name of
+#' the folder where one or more sub-directories containing variables of distinct
+#' environmental scenarios are (useful if multiple projections are needed).
+#' If RasterStack, stack of variables representing one scenario for projection.
+#' Variable names must correspond with those of \code{variables} (i.e., their
+#' names must match).
+#' @param return_projection (logical) whether to return raster PCs for
+#' projection scenario(s) as part of the resulting list. Default = FALSE.
+#' @param write_to_directory (logical) whether to write results in
+#' \code{output_directory}. Default = FALSE.
+#' @param out_format (character) format of layers to be written in
+#' \code{output_directory}. Options are "ascii", "GTiff", and "EHdr" = bil.
+#' Default = "GTiff".
 #' @param output_directory (character) name of the folder to be created to save
-#' the results of the analyses. Default = "PCA_results".
+#' results.
 #'
 #' @return
-#' A list containing PCA summary and PCA loadings as matrices;
-#' if \code{return_back} = TRUE, one or multiple (if projected)
-#' RasterStacks of principal components are returned additionally.
-#'
-#' All results are written in \code{output_directory}.
-#'
-#' @details
-#' If \code{scale} = TRUE, variables are centered to cero and scaled using
-#' \code{\link[base]{scale}}.
+#' A list containing:
+#' - PCA results
+#' - RasterStack of principal components for \code{variables}
+#' - if return_projection = TRUE, list of RasterStack objects with principal
+#' components for other scenarios. NULL if \code{project} = FALSE.
 #'
 #' @export
+#' @importFrom raster nlayers
+#'
+#' @usage
+#' pca_raster(variables, in_format = NULL, scale = TRUE, center = TRUE,
+#'            n_pcs = NULL, project = FALSE, projection_variables,
+#'            return_projection = FALSE, write_to_directory = FALSE,
+#'            out_format = "GTiff", output_directory)
 
-
-pca_raster <- function(variables, in_format = "ascii", out_format = "ascii",
-                       scale = TRUE, project = FALSE, projection_variables,
-                       return_back = FALSE, n_pcs, output_directory = "PCA_results") {
+pca_raster <- function(variables, in_format = NULL, scale = TRUE, center = TRUE,
+                       n_pcs = NULL, project = FALSE, projection_variables,
+                       return_projection = FALSE, write_to_directory = FALSE,
+                       out_format = "GTiff", output_directory) {
 
   # checking for potential errors
   if (missing(variables)) {
-    stop("Argument variables must be defined. See functions help.")
+    stop("Argument 'variables' must be defined")
+  }
+  clsv <- class(variables)[1]
+  if (!clsv %in% c("character", "RasterStack", "RasterBrick")) {
+    stop("'variables' must be of class 'character' or 'RasterStack'")
+  }
+  if (clsv == "character") {
+    if (is.null(in_format)) {
+      stop("If class of 'variables' = 'character', 'in_format' must be defined")
+    }
   }
   if (project == TRUE) {
     if (missing(projection_variables)) {
-      stop("If projections are needed, argument projection_variables must be defined. See functions help.")
+      stop("If 'project' = TRUE, argument 'projection_variables' must be defined")
+    }
+    if (return_projection == FALSE & write_to_directory == FALSE) {
+      message("Setting 'return_projection' = TRUE to keep results from projections")
+      return_projection <- TRUE
     }
   }
+  if (write_to_directory) {
+    stop("If 'write_to_directory' = TRUE, 'output_directory' must be defined")
+  }
 
-  # preparing raster formats
-  if (in_format == "ascii") {
-    patt <- ".asc$"
-  }
-  if (in_format == "GTiff") {
-    patt <- ".tif$"
-  }
-  if (in_format == "EHdr") {
-    patt <- ".bil$"
-  }
-  if (out_format == "ascii") {
-    patt1 <- ".asc"
-  }
-  if (out_format == "GTiff") {
-    patt1 <- ".tif"
-  }
-  if (out_format == "EHdr") {
-    patt1 <- ".bil"
+  # preparing things
+  patt1 <- rformat_type(out_format)
+
+  if (write_to_directory == TRUE) {
+    dir.create(output_directory)
+    pca_fol <- paste0(output_directory, "/Initial")
+    dir.create(pca_fol)
   }
 
   # reading variables
-  var <- list.files(variables, pattern = patt, full.names = TRUE)
-  variab <- raster::stack(var)
-  var_points <- na.omit(raster::values(variab))
+  if (clsv == "character") {
+    var <- list.files(variables, pattern = patt, full.names = TRUE)
+    variables <- raster::stack(var)
+  }
+  var_points <- na.omit(variables[])
+  pcra <- variables[[1]]
+  to_fill <- !is.na(pcra[])
+
+  if (is.null(n_pcs)) {n_pcs <- ncol(var_points)}
 
   # pca analyses
-  if (scale == TRUE) {
-    pca <- prcomp(var_points, center = TRUE, scale = TRUE)
-  } else {
-    pca <- prcomp(var_points, center = TRUE, scale = FALSE)
-  }
+  pca <- prcomp(var_points, center = center, scale = scale)
+  pcras <- list()
 
-  dir.create(output_directory)
-  pca_fol <- paste(output_directory, "Initial", sep = "/")
-  dir.create(pca_fol)
-
-  if (missing(n_pcs)) {n_pcs <- length(var)}
-  if (return_back == TRUE) {pcras <- list()}
-
-  cat("\nWriting raster PCs in Output folder, please wait...\n")
-  scores <- pca$x
+  message("Preparing raster PCs...")
   for (i in 1:n_pcs) {
-    pcra <- variab[[1]]
-    pcra[!is.na(raster::values(pcra))] <- scores[, i]
-    filenam <- paste(pca_fol, "/pc_", i, patt1, sep = "")
-    raster::writeRaster(pcra, filenam, format = out_format)
+    pcra[to_fill] <- pca$x[, i]
+    pcras[[i]] <- pcra
 
-    if (return_back == TRUE) {pcras[[i]] <- pcra}
+    if (write_to_directory == TRUE) {
+      filenam <- paste0(pca_fol, "/PC", i, patt1)
+      raster::writeRaster(pcra, filenam, format = out_format)
+    }
   }
 
-  if (return_back == TRUE) {
-    pcras <- do.call(raster::stack, pcras)
-    names(pcras) <- paste0("pc_", 1:dim(pcras)[3])
+  pcras <- do.call(raster::stack, pcras)
+  names(pcras) <- paste0("PC", 1:(raster::nlayers(pcras)))
+
+  SumPCAMat <- summary(pca)$importance
+
+  if (write_to_directory == TRUE) {
+    txtfile <- paste0(pca_fol, "/PCA_summary.txt")
+    cat("Principal component analysis:\n\nPCA summary\n", file = txtfile)
+    suppressWarnings(write.table(SumPCAMat, sep = "\t", file = txtfile,
+                                 append = TRUE, quote = FALSE))
+
+    cat("\n\nPCA loadings\n", file = txtfile, append = TRUE)
+    suppressWarnings(write.table(pca$rotation, sep = "\t", file = txtfile,
+                                 append = TRUE, quote = FALSE))
+
+    rd_file <- paste0(pca_fol, "/PCA_results.RData")
+    save(pca, file = rd_file)
   }
-
-  StdDev <- pca$sdev
-  VarExp <- pca$sdev^2/sum(pca$sdev^2)
-  CumVar <- cumsum(VarExp)
-  SumPCAMat <- rbind(StdDev, VarExp, CumVar)
-  colnames(SumPCAMat) <- paste("PC", seq(1, length(StdDev)), sep = "")
-  row.names(SumPCAMat) <- c("Standard deviation", "Proportion of Variance",
-                            "Cumulative Proportion")
-
-  sink(paste(paste(pca_fol, "pca_results.txt", sep = "/")))
-  cat("Principal component analysis results\n")
-  cat("\nPCA loadings\n")
-  print(pca$rotation)
-
-  cat("\n\nPCA summary\n")
-  print(SumPCAMat)
-  sink()
-
-  # pca results to be returned
-  loadings <- pca$rotation
-  respca <- SumPCAMat
 
   # projecting PCs
   if (project == TRUE) {
-    if (return_back == TRUE) {ppcrass <- list()}
+    clspr <- class(projection_variables)[1]
+    if (return_projection == TRUE) {ppcrass <- list()} ########
 
-    cat("\nProjecting and writing projected raster PCs in Output folder, please wait...\n")
-    if (class(projection_variables)[1] == "character") {
+    message("Projecting PCs to distinct scenarios...")
+    if (clspr == "character") {
       proj_dirs <- list.dirs(projection_variables, recursive = FALSE)
       proj_names <- list.dirs(projection_variables, recursive = FALSE,
                               full.names = FALSE)
       fol_names <- paste(output_directory, proj_names, sep = "/")
-    }
-    if (class(projection_variables)[1] %in% c("RasterStack", "RasterBrick")) {
+    } else {
       proj_dirs <- "projection"
       proj_names <- "Projected_PCs"
       fol_names <- paste(output_directory, proj_names, sep = "/")
     }
 
     for (h in 1:length(proj_dirs)) {
-      if (class(projection_variables)[1] == "character") {
+      if (clspr == "character") {
         pvar <- list.files(proj_dirs[h], pattern = patt, full.names = TRUE)
-        p_stack <- raster::stack(pvar)
+        projection_variables <- raster::stack(pvar)
       }
-      if (class(projection_variables)[1] %in% c("RasterStack", "RasterBrick")) {
-        p_stack <- projection_variables
-      }
-      dir.create(fol_names[h])
+      pcra <- projection_variables[[1]]
+      to_fillp <- !is.na(pcra[])
 
-      if (return_back == TRUE) {ppcras <- list()}
-      p_stackp <- na.omit(raster::values(p_stack))
-      names(p_stackp) <- names(pca[[4]])
+      if (write_to_directory == TRUE) {
+        dir.create(fol_names[h])
+      }
+
+      if (return_projection == TRUE) {ppcras <- list()}
+
+      p_stackp <- na.omit(projection_variables[])
       p_pcs <- predict(pca, newdata = p_stackp)
 
       for (i in 1:n_pcs) {
-        pcra <- p_stack[[1]]
-        pcra[!is.na(raster::values(pcra))] <- p_pcs[, i]
-        filenam <- paste(fol_names[h], "/pc_", i, patt1, sep = "")
-        raster::writeRaster(pcra, filenam, format = out_format)
+        pcra[to_fillp] <- p_pcs[, i]
+        if (return_projection == TRUE) {ppcras[[i]] <- pcra}
 
-        if (return_back == TRUE) {ppcras[[i]] <- pcra}
+        if (write_to_directory == TRUE) {
+          filenam <- paste0(fol_names[h], "/PC", i, patt1)
+          raster::writeRaster(pcra, filenam, format = out_format)
+        }
       }
 
-      if (return_back == TRUE) {
+      if (return_projection == TRUE) {
         ppcrass[[h]] <- do.call(raster::stack, ppcras)
-        names(ppcrass[[h]]) <- paste0("pc_", 1:dim(ppcrass[[h]])[3])
+        names(ppcrass[[h]]) <- paste0("PC", 1:(raster::nlayers(ppcrass[[h]])))
       }
     }
 
-    if (return_back == TRUE) {
-      names(ppcrass) <- paste("PCRasters", proj_names, sep = "_")
+    if (return_projection == TRUE) {
+      names(ppcrass) <- paste0("PCRaster_", proj_names)
     }
   }
 
-  if (return_back == TRUE) {
-    if (project == TRUE) {
-      results <- c(list(loadings, respca, pcras), ppcrass)
-      names(results)[1:3] <- c("PCA_loadings", "PCA_results", "PCRasters_initial")
-    }else {
-      results <- list(loadings, respca, pcras)
-      names(results) <- c("PCA_loadings", "PCA_results", "PCRasters_initial")
-    }
-  }else {
-    results <- list(loadings, respca)
-    names(results) <- c("PCA_loadings", "PCA_results")
+  if (project == TRUE & return_projection == TRUE) {
+    results <- list(PCA_results = pca, PCRaster_initial = pcras,
+                    PCRaster_projection = ppcrass)
+  } else {
+    results <- list(PCA_results = pca, PCRaster_initial = pcras,
+                    PCRaster_projection = NULL)
   }
 
-  cat("\nRaster PCA finished. Check your output directory",
-      paste(getwd(), output_directory, sep = "/"), "\n")
+  message("Raster PCA finished")
+  if (write_to_directory == TRUE) {
+    message("Check results in:  ", normalizePath(output_directory))
+  }
 
   return(results)
 }
